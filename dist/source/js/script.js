72beae103238a86290957d3b7d0e5c807a5191ae
Patrick510 <patrickdedinho321xz@gmail.com>
2023-11-17 14:19:38 -0400

    second-commit

--- dist/source/js/script.js
+++ dist/source/js/script.js
@@ -1,28 +1,28 @@
-// const carousel = document.querySelector(".carousel");
-// const filmes = document.querySelectorAll(".filme");
-// const dots = document.querySelectorAll(".dot");
+const carousel = document.querySelector(".carousel");
+const filmes = document.querySelectorAll(".filme");
+const dots = document.querySelectorAll(".dot");
 
-// let currentIndex = 0;
+let currentIndex = 0;
 
-// dots.forEach((dot, index) => {
-//   dot.addEventListener("click", () => {
-//     showFilmes(index * 3);
-//   });
-// });
+dots.forEach((dot, index) => {
+  dot.addEventListener("click", () => {
+    showFilmes(index * 3);
+  });
+});
 
-// function showFilmes(startIndex) {
-//   currentIndex = startIndex;
-//   updateCarousel();
-// }
+function showFilmes(startIndex) {
+  currentIndex = startIndex;
+  updateCarousel();
+}
 
-// function updateCarousel() {
-//   const newTransformValue = -currentIndex * 1;
-//   carousel.style.transform = `translateX(${newTransformValue}px)`;
+function updateCarousel() {
+  const newTransformValue = -currentIndex * 1;
+  carousel.style.transform = `translateX(${newTransformValue}px)`;
 
-//   filmes.forEach((filme, index) => {
-//     const isVisible = index >= currentIndex && index < currentIndex + 3;
-//     filme.style.display = isVisible ? "flex" : "none";
-//   });
-// }
+  filmes.forEach((filme, index) => {
+    const isVisible = index >= currentIndex && index < currentIndex + 3;
+    filme.style.display = isVisible ? "flex" : "none";
+  });
+}
 
-// updateCarousel();
+updateCarousel();
