bff16364e610bafa458129c7c94547685919397b
samuelcarlos2469 <samuelcarlos701estudos@gmail.com>
2023-11-29 23:44:20 -0400

    agr dá pra add no bd os filmes 8-)

--- dist/routes/filmes.js
+++ dist/routes/filmes.js
@@ -12,18 +12,17 @@ router.get("/", (req, res) => {
   res.render("popular");
 });
 
-router.get("/add", upload.single("imagem"), async (req, res) => {
-  //res.sendFile(__dirname + "/views/superusuario.html");
-  res.sendFile(path.resolve(__dirname, "..", "views", "insertfilme.html"));
+router.post("/add", upload.single("imagem"), async (req, res) => {
+  const filme = new FilmeModel();
 
-  // const filme = new FilmeModel();
+  const titulo = req.body.titulo;
+  const sinopse = req.body.sinopse;
+  const duracao = req.body.duracao;
+  const image = req.file.buffer.toString("base64");
+  const ano = req.body.ano;
+  const genero = req.body.genero;
 
-  // const titulo = req.body.titulo;
-  // const sinopse = req.body.sinopse;
-  // const duracao = req.body.duracao;
-  // const image = req.file.buffer.toString("base64");
-
-  // await filme.cadastrarFilme(titulo, sinopse, duracao, image);
+  await filme.cadastrarFilme(titulo, sinopse, duracao, image, ano, genero);
 });
 
 router.get("/detalhes/:id", (req, res) => {
